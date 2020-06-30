acd1af9ebee88b30529ca94f812cabe44f4a3d7c
Dmitry Shelamov <dmitryshelamov@gmail.com>
2020-06-30 23:11:52 +0700

    configure masstransit saga with extension method

---
+++ MassTransitSagaOrchestration/Booking.SagaOrchestrator/MassTransitHostConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booking.SagaOrchestrator
+{
+    class MassTransitHostConfiguration
+    {
+        public string RabbitMQAddress { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string HostName { get; set; }
+    }
+}
---
+++ MassTransitSagaOrchestration/Booking.SagaOrchestrator/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using MassTransit;
+using MassTransit.Saga;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Booking.SagaOrchestrator
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInMemorySaga(this IServiceCollection services, IConfiguration configuration)
+        {
+            var config = configuration.GetSection("MassTransitHostConfiguration")
+                .Get<MassTransitHostConfiguration>();
+            services.AddSingleton(config);
+            services.AddMassTransit(x =>
+            {
+                var saga = new BookingStateMachine();
+                var repo = new InMemorySagaRepository<BookingState>();
+
+
+                var baseUri = new Uri(config.RabbitMQAddress);
+                var hostUri = new Uri(baseUri, config.HostName);
+
+                x.AddBus(context => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                {
+                    cfg.Host(hostUri, hostCfg =>
+                    {
+                        hostCfg.Username(config.UserName);
+                        hostCfg.Password(config.Password);
+                    });
+
+                    cfg.ReceiveEndpoint(Assembly.GetExecutingAssembly().GetName().Name,
+                        e => { e.StateMachineSaga(saga, repo); });
+                }));
+                services.AddMassTransitHostedService();
+            });
+
+            return services;
+        }
+    }
+}
