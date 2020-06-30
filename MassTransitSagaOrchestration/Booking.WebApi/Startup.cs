e5e6bc59e062d148ab515dd766b71026e27c892d
Dmitry Shelamov <dmitryshelamov@gmail.com>
2020-06-30 23:17:29 +0700

    add and configure swagger
    * add swashbuckle package

--- MassTransitSagaOrchestration/Booking.WebApi/Startup.cs
+++ MassTransitSagaOrchestration/Booking.WebApi/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Booking.WebApi
 {
@@ -25,11 +27,24 @@ namespace Booking.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Booking.WebApi", Version = "1" });
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "Booking.WebApi.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Booking.WebApi");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
